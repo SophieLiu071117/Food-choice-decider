25254a705635797cf114691a8af2a35d67f4c00a
SophieLiu071117 <123265813+SophieLiu071117@users.noreply.github.com>
2024-01-13 12:20:29 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+print("Welcome to my food choice decider! \n")
+num_choices = int(input("How many choices do you want? "))
+Food_choice_list = []
+for i in range(1,num_choices+1):
+  food = str(input("Enter choice " + str(i) + ": "))
+  Food_choice_list.append(food)
+food_choice = random.choice(Food_choice_list)
+print(Food_choice_list)
+print("Your food choice is " + food_choice)
+
+  
